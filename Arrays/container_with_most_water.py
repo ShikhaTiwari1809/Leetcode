4782a6273680169c42f2c66e7eb61ed88f1471b1
ShikhaTiwari1809 <shikhat702@gmail.com>
2025-10-11 16:27:41 -0400

    container_most_water_two pointer

---
+++ Arrays/container_with_most_water.py
@@ -0,0 +1,38 @@
+""" https://leetcode.com/problems/container-with-most-water/description/?envType=study-plan-v2&envId=leetcode-75
+
+You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
+
+Find two lines that together with the x-axis form a container, such that the container contains the most water.
+
+Return the maximum amount of water a container can store.
+
+Notice that you may not slant the container.
+
+Input: height = [1,8,6,2,5,4,8,3,7]
+Output: 49
+Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the container can contain is 49.
+Example 2:
+
+Input: height = [1,1]
+Output: 1
+ 
+
+In this also we can use two pointer and find max area for each combination, wherever it is minimum change the pointer value.ValueError
+     """
+
+def maxArea(height) -> int:
+    l=0
+    r=len(height) -1
+    area = 0
+    while l < r:
+        area = max(area, min(height[l],height[r]) * (r -l))
+        if height[l] <= height [r]:
+            l = l + 1
+    
+        else:
+            r = r - 1
+        
+    return area
+
+height = [1,8,6,2,5,4,8,3,7]
+print(maxArea(height))
