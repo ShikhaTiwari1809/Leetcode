a5526aac6838811674634c8091ff21c1c77060c1
Shikha Tiwari <104676075+ShikhaTiwari1809@users.noreply.github.com>
2025-12-16 20:15:50 -0500

    Time: 15 ms (49.72%) | Memory: 20.6 MB (74.73%) - LeetSync

--- Arrays_and_string/49-group-anagrams/group-anagrams.py
+++ Arrays_and_string/49-group-anagrams/group-anagrams.py
@@ -4,10 +4,9 @@ class Solution:
         
         ans = defaultdict(list)
         for st in strs:
-            #print(''.join(sorted(set(st)))
+            
             cur_key = ''.join(sorted(st))
             ans[cur_key].append(st)
 
-        final_op = list(ans.values())
-        print(final_op)
-        return final_op
+ 
+        return list(ans.values())
