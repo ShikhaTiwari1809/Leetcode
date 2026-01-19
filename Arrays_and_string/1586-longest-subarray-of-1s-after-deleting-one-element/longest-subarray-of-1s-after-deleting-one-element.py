da64a81638f212b4fc47a83d6fa9b0d9059789f6
Shikha Tiwari <104676075+ShikhaTiwari1809@users.noreply.github.com>
2026-01-19 14:54:47 -0500

    Time: 27 ms (93.75%) | Memory: 24.4 MB (25.88%) - LeetSync

--- Arrays_and_string/1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.py
+++ Arrays_and_string/1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.py
@@ -3,15 +3,13 @@ class Solution:
         k =1
         left = 0
         max_sum = 0
-        zero_cnt = 0
+        num_delete = 0
         for right in range(len(nums)):
             if nums[right]==0:
-                zero_cnt +=1
-            if zero_cnt>k:
+                num_delete +=1
+            if num_delete>k:
                 if nums[left] ==0:
-                    zero_cnt-=1
+                    num_delete-=1
                 left+=1
-        print(right)
-        print(left)
-        print(max(zero_cnt,1))
+        
         return right - left
