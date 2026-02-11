bf4d6650a3ff2c7a29aa4f23d8b45c85a212c0be
Shikha Tiwari <104676075+ShikhaTiwari1809@users.noreply.github.com>
2026-02-11 09:15:26 -0500

    Time: 23 ms (57.74%) | Memory: 30.8 MB (59.24%) - LeetSync

--- two_pointer/1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.py
+++ two_pointer/1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.py
@@ -5,18 +5,16 @@ class Solution:
         if n-k ==0:
             return sum(cardPoints)
         
-        # start by taking all k from the left
         left_sum = sum(cardPoints[:k])
-        best = left_sum
-        right_sum = 0
+        right_sum =0
+        total_sum =left_sum+right_sum
 
-        # move 1 card at a time from left end to right end
-        for i in range(1, k + 1):
-            left_sum -= cardPoints[k - i]      # remove one from the left-picked set
-            right_sum += cardPoints[-i]        # add one from the right end
-            best = max(best, left_sum + right_sum)
+        for i in range(1,k+1):
+            left_sum -= cardPoints[k-i]
+            right_sum += cardPoints[-i]
+            total_sum = max(total_sum, left_sum+right_sum)
 
-        return best
+        return total_sum
 
 
 
