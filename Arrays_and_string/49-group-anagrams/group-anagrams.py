00852655202db11f57e112bfa081ce543d77d319
Shikha Tiwari <104676075+ShikhaTiwari1809@users.noreply.github.com>
2026-03-19 12:02:48 -0400

    Time: 14 ms (49.33%) | Memory: 23.9 MB (25.54%) - LeetSync

--- Arrays_and_string/49-group-anagrams/group-anagrams.py
+++ Arrays_and_string/49-group-anagrams/group-anagrams.py
@@ -4,9 +4,10 @@ class Solution:
         ch_map = defaultdict(list)
         
         for ch in strs:
+            count = [0] * 26
+            for c in ch:
+                count[ord(c) - ord('a')] += 1
             
-            cur_key = tuple(sorted(ch))
-            
-            ch_map[cur_key].append(ch)
+            ch_map[tuple(count)].append(ch)
         
         return list(ch_map.values())
