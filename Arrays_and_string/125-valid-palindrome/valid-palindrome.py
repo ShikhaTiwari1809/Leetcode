8d9238d82140261bd37ce6577a0d436ffdf8055e
Shikha Tiwari <104676075+ShikhaTiwari1809@users.noreply.github.com>
2026-05-07 18:03:19 -0400

    Time: 7 ms (81.23%) | Memory: 19.5 MB (94.44%) - LeetSync

--- Arrays_and_string/125-valid-palindrome/valid-palindrome.py
+++ Arrays_and_string/125-valid-palindrome/valid-palindrome.py
@@ -1,16 +1,13 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         
-        def check_alphanum(ch):
-            if (ord(ch)>=ord('A') and ord(ch)<=ord('Z')) or (ord(ch)>=ord('a') and ord(ch)<=ord('z')) or (ord(ch)>=ord('0') and ord(ch)<=ord('9')):
-                return True
         left =0
         right = len(s)-1
 
         while left<right:
-            while left<right and not check_alphanum(s[left]):
+            while left<right and not  s[left].isalnum():
                 left +=1
-            while left<right and not check_alphanum(s[right]):
+            while left<right and not  s[right].isalnum():
                 right -=1  
             
             if s[left].lower()!=s[right].lower():
