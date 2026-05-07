cffc3f8f9b64692139d4f7eba206743c6b5c8ac4
Shikha Tiwari <104676075+ShikhaTiwari1809@users.noreply.github.com>
2026-05-07 17:48:52 -0400

    Time: 27 ms (5.22%) | Memory: 24.4 MB (5.70%) - LeetSync

--- Arrays_and_string/125-valid-palindrome/valid-palindrome.py
+++ Arrays_and_string/125-valid-palindrome/valid-palindrome.py
@@ -1,10 +1,16 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-       clean_text = re.sub(r'[^a-zA-Z0-9]','',s).lower()
+        clean_text = []
+        for i in range(len(s)):
+            
+            if (ord(s[i])>=ord('A') and ord(s[i])<=ord('Z')) or (ord(s[i])>=ord('a') and ord(s[i])<=ord('z'))or (ord(s[i])>=ord('0') and ord(s[i])<=ord('9')):
+                clean_text.append(s[i].lower())
+        print(clean_text)
+        return clean_text == clean_text[::-1]
        
-       clean_text = list(clean_text)
-       
-       return clean_text == clean_text[::-1]
+       #clean_text = re.sub(r'[^a-zA-Z0-9]','',s).lower()
+       #clean_text = list(clean_text)
+       #return clean_text == clean_text[::-1]
 
        #cleaned_text = re.sub(r'[^a-zA-Z0-9]', '', s).lower()
        
