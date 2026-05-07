259f569a5d33a82b0fddb09e204bbd0a99c4dde5
Shikha Tiwari <104676075+ShikhaTiwari1809@users.noreply.github.com>
2026-05-07 19:20:27 -0400

    Time: 17 ms (26.53%) | Memory: 24.5 MB (9.87%) - LeetSync

--- Arrays_and_string/49-group-anagrams/group-anagrams.py
+++ Arrays_and_string/49-group-anagrams/group-anagrams.py
@@ -2,12 +2,10 @@ from collections import defaultdict
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         ch_map = defaultdict(list)
-        
+
         for ch in strs:
-            count = [0] * 26
-            for c in ch:
-                count[ord(c) - ord('a')] += 1
-            
-            ch_map[tuple(count)].append(ch)
-        
+            #print(tuple(ch))
+            #print(Counter(ch))
+            ch_map[tuple(sorted(ch))].append(ch)
+        print(ch_map)
         return list(ch_map.values())
