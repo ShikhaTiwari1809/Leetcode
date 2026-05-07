4d922563943997e2ad4df3530e877f1312f6359d
Shikha Tiwari <104676075+ShikhaTiwari1809@users.noreply.github.com>
2026-05-07 18:17:19 -0400

    Time: 5 ms (87.10%) | Memory: 21.5 MB (19.01%) - LeetSync

--- Arrays_and_string/125-valid-palindrome/valid-palindrome.py
+++ Arrays_and_string/125-valid-palindrome/valid-palindrome.py
@@ -1,24 +1,9 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        
-        left =0
-        right = len(s)-1
-
-        while left<right:
-            while left<right and not  s[left].isalnum():
-                left +=1
-            while left<right and not  s[right].isalnum():
-                right -=1  
-            
-            if s[left].lower()!=s[right].lower():
-                return False
-            left +=1
-            right -=1 
-        return True
        
-       #clean_text = re.sub(r'[^a-zA-Z0-9]','',s).lower()
-       #clean_text = list(clean_text)
-       #return clean_text == clean_text[::-1]
+       clean_text = re.sub(r'[^a-zA-Z0-9]','',s).lower()
+       clean_text = list(clean_text)
+       return clean_text == clean_text[::-1]
 
        #cleaned_text = re.sub(r'[^a-zA-Z0-9]', '', s).lower()
        
