98627f7bf56059c1ef582fc99df8d2eea7fedb0e
Shikha Tiwari <104676075+ShikhaTiwari1809@users.noreply.github.com>
2026-05-07 17:56:21 -0400

    Time: 21 ms (5.22%) | Memory: 19.3 MB (99.21%) - LeetSync

--- Arrays_and_string/125-valid-palindrome/valid-palindrome.py
+++ Arrays_and_string/125-valid-palindrome/valid-palindrome.py
@@ -1,12 +1,23 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        clean_text = []
-        for i in range(len(s)):
+        
+        def check_alphanum(ch):
+            if (ord(ch)>=ord('A') and ord(ch)<=ord('Z')) or (ord(ch)>=ord('a') and ord(ch)<=ord('z')) or (ord(ch)>=ord('0') and ord(ch)<=ord('9')):
+                return True
+        left =0
+        right = len(s)-1
+
+        while left<right:
+            while left<right and not check_alphanum(s[left]):
+                left +=1
+            while left<right and not check_alphanum(s[right]):
+                right -=1  
             
-            if (ord(s[i])>=ord('A') and ord(s[i])<=ord('Z')) or (ord(s[i])>=ord('a') and ord(s[i])<=ord('z'))or (ord(s[i])>=ord('0') and ord(s[i])<=ord('9')):
-                clean_text.append(s[i].lower())
-        print(clean_text)
-        return clean_text == clean_text[::-1]
+            if s[left].lower()!=s[right].lower():
+                return False
+            left +=1
+            right -=1 
+        return True
        
        #clean_text = re.sub(r'[^a-zA-Z0-9]','',s).lower()
        #clean_text = list(clean_text)
